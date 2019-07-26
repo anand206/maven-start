8e6326bf26e0b4cd48037520e31b064673ba0f67
Anandababu Bolisetti <anandababu.bolisetti@gmail.com>
2019-07-26 15:26:58 +0530

    Use apache commons stringutils and add dependency

--- src/main/java/anab/works/training/Application.java
+++ src/main/java/anab/works/training/Application.java
@@ -2,8 +2,13 @@ package anab.works.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	public int countWords(String text) {
+		String[] words = StringUtils.split(text, ' ');
+		return (words == null) ? 0: words.length;
+	}
 	
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
@@ -22,5 +27,6 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	System.out.println ("No.of Words in String \"This is Anand!\" is : " + app.countWords("This is Anand!"));
     }
 }
